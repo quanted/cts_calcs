9890612ece196d6d1e45de55e16ed9a9d2c37b73
popenc <i.nickpope@gmail.com>
2018-10-24 12:01:08 -0400

    smilesfilter fix from typo

--- smilesfilter.py
+++ smilesfilter.py
@@ -49,7 +49,7 @@ class SMILESFilter(object):
 		"""
 		# for exclude_char in self.excludestring:
 		# NOTE: Now just checking for ionic bond (i.e., ".") since CTSWS checks metals:
-		if exclude_char in self.excludestring[0]:
+		if smiles in self.excludestring[0]:
 			return False
 		return True
 
