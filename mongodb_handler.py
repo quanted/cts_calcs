280ee35dc00796924b2136555a8306264f863d71
popenc <i.nickpope@gmail.com>
2019-02-15 16:22:10 -0500

    added db connection test - defaults to webservices calls if no database

--- mongodb_handler.py
+++ mongodb_handler.py
@@ -2,9 +2,10 @@
 Handles CTS mongodb interactions.
 """
 
-from pymongo import MongoClient
+import pymongo
 import datetime
 import pytz
+import logging
 
 
 
@@ -12,11 +13,12 @@ class MongoDBHandler:
 
 	def __init__(self):
 		# MongoDB Settings:
-		self.mongodb_conn = MongoClient(port=27017)
+		self.mongodb_conn = pymongo.MongoClient(port=27017)
 		self.db = self.mongodb_conn.cts  # opens cts database
 		self.chem_info_collection = self.db.chem_info  # chem info data collection
 		# self.pchem_collection = self.db.pchem  # pchem data collection
 		# self.products_collection = self.db.products  # transformation products collection
+		self.db_conn_timeout = 1
 
 		# Keys for chem info collection document entry:
 		self.chem_info_keys = ["chemical", "orig_smiles", "smiles", "preferredName", 
@@ -24,6 +26,20 @@ class MongoDBHandler:
 
 		# Keys for pchem collection document entry:
 		# self.pchem_keys = ["smiles", "calc", "prop", "data", "method"]
+
+		self.test_db_connection()
+
+	def test_db_connection(self):
+		"""
+		Tests mongodb connection, sets is_connected to True
+		if connected.
+		"""
+		try:
+			self.mongodb_conn.server_info()
+			self.is_connected = True
+		except pymongo.errors.ServerSelectionTimeoutError as e:
+			logging.warning("Unable to connect to db.")
+			self.is_connected = False
 
 	def gen_jid(self):
 		ts = datetime.datetime.now(pytz.UTC)
@@ -41,19 +57,23 @@ class MongoDBHandler:
 				query_obj[key] = val
 		return query_obj
 
-	def find_chem_info_document(self, query_obj):
+	def find_chem_info_document(self, query_obj, is_connected=False):
 		"""
 		Searches chem info collection for document matching chemical.
 		Returns chem info data if it exists, or None if it doesn't.
 		"""
+		if not is_connected:
+			return None
 		chem_info_result = self.chem_info_collection.find_one(query_obj)  # searches db
 		return chem_info_result
 
-	def insert_chem_info_data(self, molecule_obj):
+	def insert_chem_info_data(self, molecule_obj, is_connected=False):
 		"""
 		Inserts chem info data into chem info collection.
 		Returns document unique _id.
 		"""
+		if not is_connected:
+			return None
 		chem_info_obj = self.chem_info_collection.insert_one(molecule_obj)  # inserts query object
 		return chem_info_obj
 
