a58a8897896ab4d03f1d15076c3539d3b3b54a6c
popenc <i.nickpope@gmail.com>
2023-05-24 14:38:56 -0400

    op ester workflow refactors

--- calculator_epi.py
+++ calculator_epi.py
@@ -82,6 +82,13 @@ class EpiCalc(Calculator):
             'organophosphorus ester hydrolysis 1',
             'organophosphorus ester hydrolysis 2'
         ]
+        self.cases = [
+            'case A',
+            'case B',
+            'case C',
+            'case D',
+            'case D not anhydride'
+        ]
 
 
     def getPostData(self, calc, prop, method=None):
@@ -155,121 +162,247 @@ class EpiCalc(Calculator):
             return round(value, 2)
 
 
+    def handle_cases_a_and_b(self, response_obj, route, num_sites, unique_schemes_count):
+        """
+        Handles OP Ester workflow cases A and B.
+        """
+        halfLifeValue = None
+
+        if num_sites != 1:
+            return None
+
+        for data_obj in response_obj["data"]:
+            # Accounting for cases A and B:
+            logging.info("Number of sites <= 1")
+            if route == self.op_esters[0] and data_obj["prop"] == "Kb":
+                halfLifeValue = self.round_half_life(data_obj["data"])
+                break
+            elif route == self.op_esters[1] and (data_obj["prop"] == "Ka" or data_obj["prop"] == "Kn"):
+                halfLifeValue = self.round_half_life(data_obj["data"])
+                break
+
+        return halfLifeValue
+
+
+    def handle_case_c(self, response_obj, route, num_sites, unique_schemes_count):
+        """
+        Handles OP Ester Workflow case C.
+        """
+        if not unique_schemes_count > 1:
+            logging.warning("unique_schemes_count must be greater than 1 for case C. Returning None.")
+            return None
+        logging.info("unique_schemes_count > 1")
+        logging.info("Case C: Skipping for now.")
+        pass  # don't see case C example in excel file
+
+
+    def handle_case_d(self, response_obj, route, num_sites, unique_schemes_count):
+        """
+        Handles OP Ester Workflow case D.
+        """
+
+        case = None
+
+        if unique_schemes_count > 1:
+            logging.warning("unique_schemes_count must be 1 or less for case D. Returning None.")
+            return None
+
+        for data_obj in response_obj["data"]:
+            logging.info("unique_schemes_count <= 1")
+            logging.info("Case D")
+            # Case D
+            if "halogenated aliphatics" in route.lower():
+                # return N/A
+                logging.info("Case D halogenated aliphatics")
+                halfLifeValue = "N/A"
+                break
+            elif "epoxide" in route.lower() and (data_obj["prop"] == "Ka" or data_obj["prop"] == "Kn"):
+                # return Ka/n
+                logging.info("Case D epoxide, returning Ka/n")
+                halfLifeValue = self.round_half_life(data_obj["data"])
+                break
+            
+            if route in self.cleaved_list:
+                if "anhydride" in route.lower():
+                    logging.info("Case D cleaved anhydride")
+                    # return kb1 kb2 factored in (on epi wrapper side)
+                    halfLifeValue = self.round_half_life(data_obj["data"])
+                    break
+                elif data_obj["prop"] == "Kb":
+                    logging.info("case D not anhydride, returning Kb")
+
+                    halfLifeValue1 = self.round_half_life(response_obj["data"][0]["data"])
+                    halfLifeValue2 = self.round_half_life(response_obj["data"][1]["data"])
+                    halfLifeValue = halfLifeValue1 + ", " + halfLifeValue2
+
+                    case = "case D not anhydride"
+
+                    break
+
+            elif data_obj["prop"] == "Kb":
+                logging.info("Case D not cleaved, returning Kb")
+                # return Kb
+                halfLifeValue = self.round_half_life(data_obj["data"])
+                break
+
+        return halfLifeValue, case
+
+
+    def assign_both_kb(self, child_nodes, halfLifeValue, product_count):
+        """
+        Two Kb values are returned and the first half of children get the first,
+        and the second half get the second half-life.
+        """
+        halfLifeValue1 = halfLifeValue.split(",")[0]
+        halfLifeValue2 = halfLifeValue.split(",")[1]
+        i = 0
+        qsar_responses = []
+        for child_obj in child_nodes:
+            qsar_response = {}
+            qsar_response.update(child_obj)
+            qsar_response["prop"] = "qsar"
+            qsar_response["valid"] = True
+
+            # qsar_responses.append(qsar_response)
+            if i < len(child_nodes) / 2:
+                # assign first value
+                qsar_response["data"] = halfLifeValue1
+            else:
+                # assign second value
+                qsar_response["data"] = halfLifeValue2
+
+            qsar_responses.append(qsar_response)
+
+            i += 1
+
+        return qsar_responses
+
+
+
+    def determine_halflife(self, response_obj, route, num_sites, unique_schemes_count):
+        """
+        Determines halflife based on response.
+        """
+
+        logging.warning("determine_halflife called.\nresponse_obj: {}\nroute: {}\nnum_sites: {}\nunique_schemes_count: {}".format(response_obj, route, num_sites, unique_schemes_count))
+
+        halfLifeValue = None
+        case = None
+
+        if num_sites <= 1:
+            # Cases A and B
+            halfLifeValue = self.handle_cases_a_and_b()
+
+        if unique_schemes_count > 1:
+            # Case C
+            logging.info("unique_schemes_count > 1")
+            logging.info("Case C: Skipping for now.")
+            halfLifeValue = self.handle_case_c(response_obj, route, num_sites, unique_schemes_count)
+        else:
+            # Case D
+            halfLifeValue, case = self.handle_case_d(response_obj, route, num_sites, unique_schemes_count)
+
+        if not halfLifeValue:
+            halfLifeValue = self.round_half_life(response_obj["data"][0]["data"])
+
+        return halfLifeValue, case
+
+
     def make_qsar_request(self, request_dict):
         """
         Makes requests to epi suite for half-lives.
         """
-        structure = request_dict.get('filtered_smiles')
-        route = request_dict.get('route').lower()
-        route_url = None
-        num_sites = None
+
+
+        # logging.warning("request_dict childNodes: {}".format(request_dict.get("childNodes")))
+
+        # logging.warning("childNodes getlist: {}".format(request_dict.POST.getlist("childNodes")))
+
+        structure = request_dict.get("filtered_smiles")
         unique_schemes_count = int(request_dict.get("uniqueSchemesCount"))
+        product_count = int(request_dict.get("productCount"))
+        child_nodes = request_dict.get("childNodes")  # children of a single/given parent
 
-        if not route in list(self.qsar_request_map.keys()):
-            raise Exception("Route not found.")
+        qsar_responses = []
 
-        route_url = self.qsar_request_map[route]
-        url = self.baseUrl.replace('estimated', '') + route_url
+        for child_obj in child_nodes:
 
-        if route in self.cleaved_list and route in self.op_esters:
-            logging.info("Route in cleaved list and an OP Ester.")
-            num_sites = request_dict.get("chemical").count("P")  # gets count of "P" from parent smiles
-        elif route in self.cleaved_list and not route in self.op_esters:
-            logging.info("Route in cleaved list but not OP Ester.")
-            num_sites = int(request_dict.get("productCount")) / 2
-        else:
-            logging.info("Route not in cleavd list.")
-            num_sites = int(request_dict.get("productCount"))
+            # TODO: Make request per child node
 
-        logging.info("Incoming request_dict for QSAR request: {}".format(request_dict))
-        logging.info("Number of sites: {}".format(num_sites))
-        logging.info("Request to EPI for half life:\nURL:{}\nStructure:{}".format(url, structure))
+            route = child_obj.get("routes").lower()
+            route_url = None
+            num_sites = None
 
-        # TODO: Account for OP Ester route where num_sites > 1 (cases C and D).
+            if not route in list(self.qsar_request_map.keys()):
+                raise Exception("Route not found.")
 
-        response = requests.post(url, data=json.dumps({'structure': structure}), headers=self.headers)
+            route_url = self.qsar_request_map[route]
+            url = self.baseUrl.replace("estimated", "") + route_url
 
-        if response.status_code != 200:
-            return {
-                "error": "Error getting QSAR data from EPI Suite",
-                "valid": False
-            }
+            if route in self.cleaved_list and route in self.op_esters:
+                logging.info("Route in cleaved list and an OP Ester.")
+                # num_sites = request_dict.get("chemical").count("P")  # gets count of "P" from original parent smiles
+                num_sites = structure.count("P")  # gets count of "P" from filtered parent smiles
 
-        try:
-            response_obj = json.loads(response.content)
-            # if not response_obj.get("data") or len(response_obj.get("data")) != 1:
-            if not response_obj.get("data") or len(response_obj.get("data")) < 1:
-                raise Exception("Half-life response does not have excepted 'data' key or size is unexpected.\nResponse: {}".format(response_obj))
-            
-            # NOTE: If OP Ester 1/2 and num_sites <= 1, pick specific half-life value from set.
-            # Get Kb if OP Ester 1, and Ka/n for OP Ester 2
+            elif route in self.cleaved_list and not route in self.op_esters:
+                logging.info("Route in cleaved list but not OP Ester.")
+                num_sites = product_count / 2
+            else:
+                logging.info("Route not in cleavd list.")
+                num_sites = product_count
 
-            logging.info("Half-life response data: {}".format(response_obj["data"]))
+            logging.info("Incoming child_obj for QSAR request: {}".format(child_obj))
+            logging.info("Number of sites: {}".format(num_sites))
+            logging.info("Request to EPI for half life:\nURL:{}\nStructure:{}".format(url, structure))
 
-            halfLifeValue = None
-            for data_obj in response_obj["data"]:
-                if num_sites <= 1:
-                    logging.info("Number of sites <= 1")
-                    if route == self.op_esters[0] and data_obj["prop"] == "Kb":
-                        halfLifeValue = self.round_half_life(data_obj["data"])
-                        break
-                    elif route == self.op_esters[1] and (data_obj["prop"] == "Ka" or data_obj["prop"] == "Kn"):
-                        halfLifeValue = self.round_half_life(data_obj["data"])
-                        break
-                else:
-                    logging.info("Number of sites > 1")
-                    # Case C or D
-                    if unique_schemes_count > 1:
-                        logging.info("unique_schemes_count > 1")
-                        # Case C
-                        pass  # don't see case C example in excel file
-                    else:
-                        logging.info("unique_schemes_count <= 1")
-                        # Case D
-                        if "halogenated aliphatics" in route.lower():
-                            # return N/A
-                            logging.info("Case D halogenated aliphatics")
-                            halfLifeValue = "N/A"
-                            break
-                        elif "epoxide" in route.lower() and (data_obj["prop"] == "Ka" or data_obj["prop"] == "Kn"):
-                            # return Ka/n
-                            logging.info("Case D epoxide, returning Ka/n")
-                            halfLifeValue = self.round_half_life(data_obj["data"])
-                            break
-                        
-                        if route in self.cleaved_list:
-                            if "anhydride" in route.lower():
-                                logging.info("Case D cleaved anhydride")
-                                # return kb1 kb2 factored in (on epi wrapper side)
-                                halfLifeValue = self.round_half_life(data_obj["data"])
-                                break
-                            elif data_obj["prop"] == "Kb":
-                                logging.info("Case D not anhydride, returning Kb")
-                                # return Kb
-                                halfLifeValue = self.round_half_life(data_obj["data"])
-                                break
+            # TODO: Account for OP Ester route where num_sites > 1 (cases C and D).
 
-                        elif data_obj["prop"] == "Kb":
-                            logging.info("Case D not cleaved, returning Kb")
-                            # return Kb
-                            halfLifeValue = self.round_half_life(data_obj["data"])
-                            break
+            response = requests.post(url, data=json.dumps({'structure': structure}), headers=self.headers)
 
-            if not halfLifeValue:
-                halfLifeValue = self.round_half_life(response_obj["data"][0]["data"])
+            if response.status_code != 200:
+                return {
+                    "error": "Error getting QSAR data from EPI Suite",
+                    "valid": False
+                }
 
-            response_obj["data"][0]["data"] = halfLifeValue
+            try:
+                response_obj = json.loads(response.content)
+                # if not response_obj.get("data") or len(response_obj.get("data")) != 1:
+                if not response_obj.get("data") or len(response_obj.get("data")) < 1:
+                    raise Exception("Half-life response does not have excepted 'data' key or size is unexpected.\nResponse: {}".format(response_obj))
+                
+                # NOTE: If OP Ester 1/2 and num_sites <= 1, pick specific half-life value from set.
+                # Get Kb if OP Ester 1, and Ka/n for OP Ester 2
 
-            return {
-                "data": response_obj,
-                "valid": True
-            }
-        except Exception as e:
-            logging.warning("Error parsing QSAR response: {}".format(e))
-            return {
-                "error": "Error getting QSAR data from EPI Suite",
-                "valid": False
-            }
+                # qsar_response = {}
+                # qsar_response.update(child_obj)
+
+                halfLifeValue, case = self.determine_halflife(response_obj, route, num_sites, unique_schemes_count)
+
+                if case == "case D not anhydride":
+                    # Assign HL1 to first half of children, HL2 to second half, no need to 
+                    # continue looping and requesting.
+                    logging.info("case D not anhydride.")
+                    qsar_responses = self.assign_both_kb(child_nodes, halfLifeValue, product_count)
+                    return qsar_responses
+
+                qsar_response = {}
+                qsar_response.update(child_obj)
+                qsar_response["data"] = halfLifeValue
+                qsar_response["prop"] = "qsar"
+                qsar_response["valid"] = True
+
+                qsar_responses.append(qsar_response)
+
+            except Exception as e:
+                logging.warning("Error parsing QSAR response: {}".format(e))
+                qsar_response = {}
+                qsar_response["error"] = "Error getting QSAR data from EPI Suite"
+                qsar_response["valid"] = False
+                qsar_responses.append(qsar_response)
+
+
+        return qsar_responses
 
 
     def data_request_handler(self, request_dict):
@@ -305,7 +438,9 @@ class EpiCalc(Calculator):
 
             # TODO: Account for not valid result_obj
 
-            _response_dict.update(_result_obj['data'])
+            _response_dict['data'] = _result_obj
+
+            # _response_dict.update(_result_obj['data'])
             _response_dict['valid'] = True
 
             return _response_dict
