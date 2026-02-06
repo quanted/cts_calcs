0d545d2d691b3bc0b9776814a0b1c9c6dbe9deed
popenc <i.nickpope@gmail.com>
2026-02-06 09:08:14 -0500

    fixed timestamp function

--- calculator.py
+++ calculator.py
@@ -136,8 +136,8 @@ class Calculator(object):
 			return "Error: result key not found"
 
 	def gen_jid(self):
-		ts = datetime.now(timezone.UTC)
-		localDatetime = ts.astimezone(ZoneInfo.timezone('US/Eastern'))
+		ts = datetime.now(timezone.utc)
+		localDatetime = ts.astimezone(ZoneInfo('US/Eastern'))
 		jid = localDatetime.strftime('%Y%m%d%H%M%S%f')
 		return jid
 
